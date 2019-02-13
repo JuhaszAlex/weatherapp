5dd4edbbb602b4a7f53203fa49593e7144033971
JuhaszAlex <juhaszalex95@gmail.com>
2019-02-13 11:23:12 +0100

    Updates

---
+++ precache-manifest.62f8b8dfb4f152938c1afe06de843343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7ea501e7b8f22ce0b7",
+    "url": "/weatherapp/static/js/runtime~main.0e7ea501.js"
+  },
+  {
+    "revision": "274fbe5bbfa25ca54536",
+    "url": "/weatherapp/static/js/main.274fbe5b.chunk.js"
+  },
+  {
+    "revision": "dc815e50707f800c0f8c",
+    "url": "/weatherapp/static/js/1.dc815e50.chunk.js"
+  },
+  {
+    "revision": "274fbe5bbfa25ca54536",
+    "url": "/weatherapp/static/css/main.c6d797b7.chunk.css"
+  },
+  {
+    "revision": "dc815e50707f800c0f8c",
+    "url": "/weatherapp/static/css/1.884a2fa1.chunk.css"
+  },
+  {
+    "revision": "c6a1fb65c3258d7c6a9c85b23230e0ac",
+    "url": "/weatherapp/index.html"
+  }
+];
